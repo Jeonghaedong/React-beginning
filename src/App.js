58c25aaf7adbf7a99aff126255d5968d1ce46d88
jeong <abit0718@naver.com>
2021-07-29 12:47:09 +0900

    배열 ref까지

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Hello from './hello';
 import InputSample from './InputSample';
+import UserList from './UserList';
 
 
 function App() {
+  const users =[
+    {
+        id :1,
+        username : 'velopert',
+        email: 'public.velopert@gmail.com'
+    },
+    {
+        id :2,
+        username : 'tester',
+        email: 'tester@example.com'
+    },
+    {
+        id :3,
+        username : 'lis',
+        email : 'lis@gmail.com'
+    }
+
+    
+];
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    console.log(nextId.current);
+    nextId.current += 1;
+  }
   return (
-    <InputSample />
+    <UserList users={users}/>
   )
 }
 
