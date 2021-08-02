143ae3296b5c51d7627ff40168b1604687c5b20c
jeong <abit0718@naver.com>
2021-08-02 14:19:35 +0900

    TOGGEL_USER,REMOVE_USER

--- src/App.js
+++ src/App.js
@@ -44,11 +44,25 @@ function reducer(state, action){
           [action.name]: action.value
         }
       };
-      case 'CREATE_USER':
+    case 'CREATE_USER':
       return{
         inputs : initialState.inputs,
         users : state.users.concat(action.user)
       }
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user =>
+          user.id === action.id
+          ? {...user, active: !user.active} : user
+          )
+        };
+    case 'REMOVE_USER':
+        return {
+          ...state,
+          users: state.users.filter(user => user.id !== action.id)
+        }
+      
       default : throw new Error('Unhandled action');
   }
 }
@@ -58,15 +72,16 @@ function App() {
   const { users } = state;
   const { username, email } =state.inputs;
 
-  const onChange = useCallback( e => { //
-    const {name, value} = e.target; // 이름과 값을 추출
-    dispatch({ //어떤액션을 주는가
+  const onChange = useCallback( e => { 
+    const {name, value} = e.target; 
+    // 이름과 값을 추출
+    dispatch({ 
+      //어떤액션을 주는가
       type: 'CHANGE_INPUT',
       name,
       value
     })
   }, []);
-
   const onCreate = useCallback(() => {
     dispatch({
       type: 'CREATE_USER',
@@ -77,12 +92,28 @@ function App() {
       }
     });
     nextId.current += 1;
-  },[username, email])
+  },[username, email]);
+
+  const onToggle = useCallback (id => {
+    dispatch({
+      type : 'TOGGLE_USER',
+      id
+    });
+  }, []);
+
+  const onRemove = useCallback( id=> {
+    dispatch({
+      type : 'REMOVE_USER',
+      id
+    });
+  } , []);
+
+  const count = useMemo(() => countActiveUsers(users), [users])
   return (
     <>
-    <CreateUser username={username} email={email} onChange={onchange} onCreate={onCreate} />
-    <UserList users={users} />
-    <div>활성자수 : 0 </div>
+    <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+    <UserList users={users} onToggle={onToggle} onRemove={onRemove}/>
+    <div>활성자수 : {count} </div>
     </>
   )
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,49 +1,126 @@
-import React, { useEffect }from 'react'
+import React, { useRef, useReducer, useMemo, useCallback } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
-function User({ user, onRemove, onToggle }){
-    const { username, email, id, active } = user;
-    useEffect(() => {
-        console.log('user값이 설정됨');
-        console.log(user);
-        return () =>{
-            console.log('user 값이 바뀌기전');
-            console.log(user);
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
+}
+
+const initialState = {
+  inputs: {
+    username: '',
+    email: ''
+  },
+  users: [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
+  ]
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
         }
-    }, [user]);
-    return (
-        <div>
-            <b style={{
-                color: active ? 'green' : 'blue',
-                cursor : 'pointer'
-                }}
-            onClick={ () => onToggle(id) }
-            >
-                {username}
-            </b>
-            &nbsp;
-            <span>({email})</span>
-                <button onClick={ () => onRemove(id)}>삭제</button>
-            </div>
-    );
+      };
+    case 'CREATE_USER':
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      };
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        )
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      };
+    default:
+      return state;
+  }
 }
 
-function UserList({ users, onRemove, onToggle }){
-    
-    return (
-        <div>
-            {
-                users.map(
-                    (user) => (
-                        <User 
-                            user={user} 
-                            key={user.id} 
-                            onRemove={onRemove}
-                            onToggle={onToggle}
-                        />)
-                )
-            }            
-        </div>
-    )
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const nextId = useRef(4);
+
+  const { users } = state;
+  const { username, email } = state.inputs;
+
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    });
+  }, []);
+
+  const onCreate = useCallback(() => {
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
+    });
+    nextId.current += 1;
+  }, [username, email]);
+
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+  }, []);
+
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  }, []);
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+      <div>활성사용자 수 : {count}</div>
+    </>
+  );
 }
 
-export default UserList;
+export default App;
